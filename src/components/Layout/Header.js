f568696496304c160214f51df5cfcb62032d0cf1
Lisa Crowell <lisa.l.crowell@gmail.com>
2022-03-12 09:10:55 -0600

    create CartIcon, import it to HeaderCartButton.js and tie HeaderCart into Header import image

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import {Fragment} from "react";
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
 
 export default function Header(props) {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>Tasty Meals to Go!</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt='A table full of delicious food!' />
