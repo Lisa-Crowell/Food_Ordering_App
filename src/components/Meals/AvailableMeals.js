1e78a24021a7083240eb2aab91b97ef6e39d8f24
Lisa Crowell <lisa.l.crowell@gmail.com>
2022-03-12 11:27:53 -0600

    fill out MealItem.js begin working on MealItemForm.js import css modules from Udemy github provided

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,39 +1,50 @@
 import classes from './AvailableMeals.module.css'
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 const DUMMY_MEALS = [
     {
         id: 'm1',
         name: 'Sushi',
-        description: 'Finest fish and veggies',
+        description: 'Finest, fresh fish and gourmet veggies served with rice and soup.',
         price: 22.99,
     },
     {
         id: 'm2',
         name: 'Schnitzel',
-        description: 'A german specialty!',
+        description: 'A german specialtyservered with the finest lager! (only for those over 18 years old)',
         price: 16.5,
     },
     {
         id: 'm3',
         name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
+        description: 'American quarter pound burger cooked to perfection, anyway you like it!',
         price: 12.99,
     },
     {
         id: 'm4',
         name: 'Green Bowl',
-        description: 'Healthy...and green...',
+        description: 'Healthy...and green... OR you can make it less healthy by adding toppings of your choice!',
         price: 18.99,
     },
 ];
 
 export default function AvailableMeals() {
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map((meal) => (
+        <MealItem
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />
+    ));
 
     return (
     <section className={classes.meals}>
+        <Card>
         <ul>
             {mealsList}
         </ul>
+        </Card>
     </section>
     )
 };
