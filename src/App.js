88b096b2cc37f84d84e3edebfa06b6a925973252
Lisa Crowell <lisa.l.crowell@gmail.com>
2022-03-12 16:26:15 -0600

    updating order by adding to Cart no pricing or removal yet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Header from "./components/Layout/Header";
-import {Fragment, useState} from "react";
+import {useState} from "react";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 export default function App() {
     const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,13 +16,13 @@ export default function App() {
     };
 
     return (
-        <Fragment>
+        <CartProvider>
             {cartIsShown && <Cart onClose={hideCartHandler}/>}
             <Header onShowCart={showCartHandler}/>
             <main>
                 <Meals/>
             </main>
-        </Fragment>
+        </CartProvider>
     );
 }
 
