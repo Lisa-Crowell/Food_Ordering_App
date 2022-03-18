e737b28d1ba1f68821fdd552855deafe6a49976d
Lisa Crowell <lisa.l.crowell@gmail.com>
2022-03-18 17:20:32 -0500

    add the Firebase API for meals implement Error and Error catch begin Checkout process

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,39 @@
 import classes from './HeaderCartButton.module.css';
 import CartIcon from "../Cart/CartIcon";
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import CartContext from "../../store/cart-context";
 
 
 export default function HeaderCartButton(props) {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
     const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    const {items} = cartCtx;
+
+    const numberOfCartItems = items.reduce((currentNumber, item) => {
         return currentNumber + item.amount;
     }, 0);
 
-    return <button className={classes.button} onClick={props.onClick}>
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if(cartCtx.items.length === 0){
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false)
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
-            <CartIcon />
+            <CartIcon/>
         </span>
         <span>Your Cart</span>
         <span className={classes.badge}>{numberOfCartItems}</span>
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,35 +1,61 @@
 import classes from './AvailableMeals.module.css'
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest, fresh fish and gourmet veggies served with rice and soup.',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty served with the finest lager! (Lager only for those over 18 years old.)',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American quarter pound burger cooked to perfection, anyway you like it!',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green... OR you can make it less healthy by adding toppings of your choice!',
-        price: 18.99,
-    },
-];
+import {useEffect, useState} from "react";
+
 
 export default function AvailableMeals() {
-    const mealsList = DUMMY_MEALS.map((meal) => (
+    const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState();
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('https://reat-http-e327e-default-rtdb.firebaseio.com/meals.json');
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const responseData = await response.json();
+
+            const loadedMeals = [];
+
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price,
+                });
+            }
+
+            setMeals(loadedMeals);
+            setIsLoading(false);
+        };
+
+        fetchMeals().catch((error) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        });
+    }, []);
+
+    if (isLoading) {
+        return (
+            <section className={classes.MealsLoading}>
+                <p>Loading...</p>
+            </section>
+        );
+    }
+
+    if (httpError) {
+        return (
+            <section className={classes.MealsError}>
+                <p>{httpError}</p>
+            </section>
+        );
+    }
+
+    const mealsList = meals.map((meal) => (
         <MealItem
             key={meal.id}
             id={meal.id}
@@ -40,12 +66,41 @@ export default function AvailableMeals() {
     ));
 
     return (
-    <section className={classes.meals}>
-        <Card>
-        <ul>
-            {mealsList}
-        </ul>
-        </Card>
-    </section>
-    )
+        <section className={classes.meals}>
+            <Card>
+                <ul>{mealsList}</ul>
+            </Card>
+        </section>
+    );
 };
+
+
+// Dummy meals is no longer needed as we added the meals to firebase and are now getting them through
+// the Firebase API initially DummyMeals was used as a placeholder as you might use during set up/mock up
+// before the database or backend is running for real production
+// const DUMMY_MEALS = [
+// {
+//         id: 'm1',
+//         name: 'Sushi',
+//         description: 'Finest, fresh fish and gourmet veggies served with rice and soup.',
+//         price: 22.99,
+//     },
+//     {
+//         id: 'm2',
+//         name: 'Schnitzel',
+//         description: 'A german specialty served with the finest lager! (Lager only for those over 18 years old.)',
+//         price: 16.5,
+//     },
+//     {
+//         id: 'm3',
+//         name: 'Barbeque Burger',
+//         description: 'American quarter pound burger cooked to perfection, anyway you like it!',
+//         price: 12.99,
+//     },
+//     {
+//         id: 'm4',
+//         name: 'Green Bowl',
+//         description: 'Healthy...and green... OR you can make it less healthy by adding toppings of your choice!',
+//         price: 18.99,
+//     },
+// ];
